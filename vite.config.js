cb34c335c28d82061c61ca05b96fc8d1fc967e23
Rushikesh Patil <prushikeh@gmail.com>
2026-04-19 12:31:28 +0530

    refactor: remove unnecessary server and preview host configurations from Vite setup

--- vite.config.js
+++ vite.config.js
@@ -8,12 +8,4 @@ export default defineConfig({
             '@': path.resolve(__dirname, './src')
         }
     },
-    server: {
-        host: '0.0.0.0',
-        port: 5173
-    },
-    preview: {
-        host: '0.0.0.0',
-        port: 4173
-    }
 });
